5ba2283bc24cd9d31d35571a6288959e4155b2e7
MDP\lam.nguyen <lam.nguyen@mdpweb.net>
2021-05-13 16:25:59 -0400

    Author UI

--- BookStore-API/Startup.cs
+++ BookStore-API/Startup.cs
@@ -79,13 +79,13 @@ namespace BookStore_API
                 var xpath = Path.Combine(AppContext.BaseDirectory, xfile);
                 c.IncludeXmlComments(xpath);
             });
-            
-
+        
             services.AddSingleton<ILoggerService, LoggerService>();
             services.AddScoped<IAuthorRepository, AuthorRepository>();
             services.AddScoped<IBookRepository, BookRepository>();
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(i=>
+            i.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
